c66ec94a33e87712ea7ef60f9935692d96320102
PROG-404 <appera8900@gmail.com>
2025-01-10 20:43:05 +0300

    Update Blox.lua

--- Blox.lua
+++ Blox.lua
@@ -1,1 +1,43 @@
+--[[
+ BETA FOR PROG DON'T LOOK FOR SCRIPT 😑
+]]
+local _ENV = (getgenv or getrenv or getfenv)()
 
+local VirtualInputManager = game:GetService("VirtualInputManager")
+local CollectionService = game:GetService("CollectionService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TeleportService = game:GetService("TeleportService")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+
+local Remotes = ReplicatedStorage:WaitForChild("Remotes")
+local Validator2 = Remotes:WaitForChild("Validator2")
+local Validator = Remotes:WaitForChild("Validator")
+local CommF = Remotes:WaitForChild("CommF_")
+local CommE = Remotes:WaitForChild("CommE")
+
+local ChestModels = workspace:WaitForChild("ChestModels")
+local WorldOrigin = workspace:WaitForChild("_WorldOrigin")
+local Characters = workspace:WaitForChild("Characters")
+local SeaBeasts = workspace:WaitForChild("SeaBeasts")
+local Enemies = workspace:WaitForChild("Enemies")
+local Map = workspace:WaitForChild("Map")
+
+local EnemySpawns = WorldOrigin:WaitForChild("EnemySpawns")
+local Locations = WorldOrigin:WaitForChild("Locations")
+
+local RenderStepped = RunService.RenderStepped
+local Heartbeat = RunService.Heartbeat
+local Stepped = RunService.Stepped
+local Player = Players.LocalPlayer
+
+local Modules = ReplicatedStorage:WaitForChild("Modules")
+local Net = Modules:WaitForChild("Net")
+
+local executor = if identifyexecutor then identifyexecutor() else "Null"
+local is_blacklisted_executor = table.find({ "Null", "Xeno", "Swift" }, executor)
+
+local hookmetamethod = (not is_blacklisted_executor and hookmetamethod) or (function(...) return ... end)
+local sethiddenproperty = sethiddenproperty or (function(...) return ... end)
+local setupvalue = setupvalue or (debug and debug.setupvalue)
+local getupvalue = getupvalue or (debug and debug.getupvalue)
